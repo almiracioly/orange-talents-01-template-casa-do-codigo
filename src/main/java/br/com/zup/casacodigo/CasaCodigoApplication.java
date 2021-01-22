b67d34dd57df99fee0356912ae4b1a0af4725bc6
almiracioly <almir.acioly@gmail.com>
2021-01-22 11:04:01 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/zup/casacodigo/CasaCodigoApplication.java
@@ -0,0 +1,15 @@
+package br.com.zup.casacodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class CasaCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaCodigoApplication.class, args);
+	}
+	
+}
